368e27f20f05616d19c7ad294a0147730b0663a0
safitriayu <ayuvanpersie20@gmail.com>
2022-03-21 21:06:17 +0700

    menambahkan file index

---
+++ pustaka-booking/application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller {
+    public function index() {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Ayu Safitri
+              Saya tinggal di daerah Jakarta Selatan
+              Olahraga yang saya sukai adalah Bulutangkis";
+    }
+}
