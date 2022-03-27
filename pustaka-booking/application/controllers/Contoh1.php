478d3888308e622cfb40795ce11b83b32df9b433
safitriayu <ayuvanpersie20@gmail.com>
2022-03-27 16:30:02 +0700

    menambahkan file

--- pustaka-booking/application/controllers/Contoh1.php
+++ pustaka-booking/application/controllers/Contoh1.php
@@ -4,6 +4,6 @@ class Contoh1 extends CI_Controller {
         echo "<h1>Perkenalkan</h1>";
         echo "Nama saya Ayu Safitri
               Saya tinggal di daerah Jakarta Selatan
-              Olahraga yang saya sukai adalah Bulutangkis";
+              Olahraga yang saya sukai adalah berlari lari dipikiranmu";
     }
 }
---
+++ pustaka-booking/application/views/view-latihan.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Latihan 1</title>
+</head>
+<body>
+    Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+    Nilai 1 = <?= $nilai1; ?> <br>
+    Nilai 2 = <?= $nilai2; ?> <br>
+    ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
