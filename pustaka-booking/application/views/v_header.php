eb726ffaa35cb9512122cb9172107fcdbd7ac3bd
safitriayu <ayuvanpersie20@gmail.com>
2022-04-03 21:20:49 +0700

    menambahkan file

---
+++ pustaka-booking/application/views/v_header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Web Prog II | Merancang Template Sederhana dengan Codeigniter</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() 
+    ?>assets/css/stylebuku.css">
+</head>
+<body>
+    <div id="wrapper">
+        <header>
+            <hgroup>
+                <h1>RentalBuku.net</h1>
+                <h3>Membuat Template Sederhana dengan Codeigniter</h3>
+</hgroup>
+<nav>
+    <ul>
+        <li><a href="<?php echo 
+        base_url().'index.php/web' ?>">Home</a></li>
+        <li><a href="<?php echo 
+        base_url().'index.php/web/about' ?>">About</a></li>
+            </ul>
+</nav>
+<div class="clear"></div>
+</header>
+    
+</body>
+</html>
---
+++ pustaka-booking/application/views/view-form-matakuliah.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form Input Matakuliah</title>
+</head>
+<body>
+    <?php echo validation_errors(); ?>
+    <center>
+        <form action="<?= base_url('matakuliah/cetak'); ?>"
+        method="post">
+            <table>
+            <tr>
+            <th colspan="3">
+                Form Input Data Mata Kuliah
+            </th>
+        </tr>
+        <tr>
+            <td colspan="3">
+                <hr>
+</td>
+</tr>
+<tr>
+    <th>Kode MTK</th>
+    <th>:</th>
+    <td>
+        <input type="text" name="kode" id="kode">
+</td>
+</tr>
+<tr>
+    <th>Nama MTK</th>
+    <td>:</td>
+    <td>
+        <input type="text" name="nama" id="nama">
+</td>
+</tr>
+<tr> 
+    <th>SKS</th>
+    <td>:</td>
+    <td>
+        <select name="sks" id="sks">
+            <option value="">Pilih SKS</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+</select>
+</td>
+</tr>
+<tr>
+    <td colspan="3" align="center">
+        <input type="Submit" value="Submit">
+</td>
+</tr>
+</table>
+</form>
+</center>
+
+</body>
+</html>
