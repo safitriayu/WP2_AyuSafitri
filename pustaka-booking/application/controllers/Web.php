8b959d94df58fdb3877355d29ad11d45b4751e98
safitriayu <ayuvanpersie20@gmail.com>
2022-04-05 17:17:47 +0700

    menambahkan file

--- pustaka-booking/application/controllers/Web.php
+++ pustaka-booking/application/controllers/Web.php
@@ -13,4 +13,10 @@ class Web extends CI_Controller{
         $this->load->view('v_index',$data);
         $this->load->view('v_footer',$data);
     }
+    public function about(){
+        $data['judul'] = "Halaman About";
+        $this->load->view('v_header',$data);
+        $this->load->view('v_about',$data);
+        $this->load->view('v_footer',$data);
+    }
 }
--- pustaka-booking/application/views/v_header.php
+++ pustaka-booking/application/views/v_header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <title>Web Prog II | Merancang Template Sederhana dengan Codeigniter</title>
@@ -15,14 +15,10 @@
 </hgroup>
 <nav>
     <ul>
-        <li><a href="<?php echo 
-        base_url().'index.php/web' ?>">Home</a></li>
-        <li><a href="<?php echo 
-        base_url().'index.php/web/about' ?>">About</a></li>
-            </ul>
+        <li><a href="<?php echo base_url().'index.php/web' ?>">Home</a></li>
+        <li><a href="<?php echo base_url().'index.php/web/about' ?>">About</a></li>
+    </ul>
 </nav>
 <div class="clear"></div>
 </header>
     
-</body>
-</html>
